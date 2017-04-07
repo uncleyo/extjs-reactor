b7de5e75f6750fa7b140a3a9364119cdaa3be231
Mark Brocato <mark.brocato@sencha.com>
2017-04-07 03:30:17 -0400

    stuff

--- packages/reactor-fiber/src/index.js
+++ packages/reactor-fiber/src/index.js
@@ -1,15 +1,22 @@
 import ReactDOM from 'react-dom';
 import ExtReactProvider from './ExtReactProvider';
 
+let settings;
+
 /**
  * Launches an ExtReact application, creating a viewport and rendering the specified root component into it.
  * @param {React.Component} rootComponent You application's root component
+ * @param {Object} [appConfig] Additional config parameters for Ext.application
  */
-function launch(rootComponent) {
+export function launch(rootComponent, appConfig = { }, reactorSettings = { debug: false }) {
+    settings = reactorSettings;
+
     Ext.application({
+        name: '$ExtReactApp',
+        ...appConfig,
         launch: () => ReactDOM.render(rootComponent, Ext.Viewport.getRenderTarget().dom)
-    })
-}
+    });
+} 
 
 module.exports = {
     launch, 
